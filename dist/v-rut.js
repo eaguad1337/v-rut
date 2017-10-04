17b26680f02a90199b9649714f7be49c9d95b719
Eduardo Aguad <eduaguad@gmail.com>
2017-10-04 10:05:59 -0300

    Update dist

--- dist/v-rut.js
+++ dist/v-rut.js
@@ -110,6 +110,7 @@ const vRutDirective = {
  * @return {string} The formatted Rut
  */
 function format(val) {
+  if (!val.length) return '';
   return clean(val).slice(0, -1).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.") + '-' + clean(val).slice(-1);
 }
 
