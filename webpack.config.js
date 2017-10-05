6b7fd6283903c18e69450fe958592a1a58298029
Eduardo Aguad <eduaguad@gmail.com>
2017-10-05 18:37:30 -0300

    Webpack librarytarget umd

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
 	entry: __dirname + '/src/v-rut.js',
 	output: {
 		path: __dirname + '/dist',
-		filename: 'v-rut.js'
+		filename: 'v-rut.js',
+		libraryTarget: 'umd'
 	},
 	module: {
 		loaders: [{
